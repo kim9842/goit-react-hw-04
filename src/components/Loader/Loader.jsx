90e79ab35915e567c656632732597f17ad48c3af
kim9842 <61659301+kim9842@users.noreply.github.com>
2025-06-09 19:26:58 +0300

    try

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
 import s from "./Loader.module.css";
-import { HashLoader } from "react-spinners";
+import { BarLoader } from "react-spinners";
 
 const Loader = () => {
   return (
     <div className={s.loader}>
-      <HashLoader size={50} color="#36d7b7" />
+      <BarLoader size={50} color="black" />
     </div>
   );
 };
